ec8a734c99d14ff6bb1dda9f2e9d77099b8f53f9
phamxuanloc0 <locpham2307@gmail.com>
2021-09-28 00:52:34 +0700

    add filter test permisson

--- src/main/java/com/example/spring_boot_jwt_phamxuanloc/controller/AuthController.java
+++ src/main/java/com/example/spring_boot_jwt_phamxuanloc/controller/AuthController.java
@@ -13,23 +13,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class AuthController {
-
     @Autowired
     private UserService userService;
 
     @Autowired
-    private JwtUtil jwtUtil;
+    private TokenService tokenService;
 
     @Autowired
-    private TokenService tokenService;
+    private JwtUtil jwtUtil;
 
     @PostMapping("/register")
     public User register(@RequestBody User user){
@@ -65,7 +61,20 @@ public class AuthController {
     @GetMapping("/hello")
     @PreAuthorize("hasAnyAuthority('USER_READ')")
     public ResponseEntity hello(){
-        return ResponseEntity.ok("hello");
+        return ResponseEntity.ok("hello mother !!");
+    }
+
+    @PutMapping("/user")
+    @PreAuthorize("hasAnyAuthority('USER_UPDATE')")
+    public ResponseEntity update(){
+        return ResponseEntity.ok("Đây là update nha !");
+    }
+
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAnyAuthority('ADMIN_DELETE')")
+    public ResponseEntity hello1(){
+        return ResponseEntity.ok("hello mother !!");
     }
 
 }
